380f0605be5f5341a27fdd774e61186792dc200a
Jonathan_Meixueiro <lav.jonathanom@gmail.com>
2025-01-30 08:51:45 -0800

    Lazy stored properties

--- properties.playground/Contents.swift
+++ properties.playground/Contents.swift
@@ -12,3 +12,22 @@ rangeOfThreeItems.firstValue = 6
 let rangeOfFourItems = FixedLengthRange(firstValue: 0, lenght: 4)
 
 // rangeOfFourItems.firstValue = 5 -> this throws an error, because it is declared as constant, even when the property is var
+
+class DataImporter {
+    var fileName = "data.txt"
+}
+
+class DataManager {
+    lazy var importer = DataImporter()
+    var data = [String]()
+}
+
+let manager = DataManager()
+manager.data.append("Some data")
+manager.data.append("More data")
+// Importer is not initialized here,
+manager
+// Importer is created right here, because it is not created until it is called
+manager.importer.fileName
+// Here we can see it already initialized
+manager
